f8482ce2e4232820ac9b1a0a9d04cea23b427d0c
yuri guemra <yuri.guemra@live.fr>
2023-04-18 13:49:56 +0200

    tp

--- fitness/components/FitnessCard.js
+++ fitness/components/FitnessCard.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View , Pressable, Image} from "react-native";
 import React from "react";
 import fitness from "../data/fitness";
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const FitnessCards =() => {
@@ -11,6 +12,7 @@ const FitnessCards =() => {
                     <Pressable style={{alignItems:'center',justifyContent:'center',margin:10,}} key={key}>
                         <Image style={{width:'95%',height:140, borderRadius:7}} source={{uri:item.image}}></Image>
                         <Text style={{position:'absolute',fontSize:16,fontWeight:'bold',left:30,top:25}}>{item.name}</Text>
+                        <MaterialIcons style={{position:'absolute',bottom:15,left:20}} name="offline-bolt" size={24} color="yellow" />
                     </Pressable>
                 ))}
         </View>
