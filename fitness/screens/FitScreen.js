ee41c7599c3359dfbd1ea1fcd67159a568fa70e4
yuri guemra <yuri.guemra@live.fr>
2023-04-19 10:27:15 +0200

    tp

--- fitness/screens/FitScreen.js
+++ fitness/screens/FitScreen.js
@@ -1,20 +1,74 @@
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image,Pressable } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import { useState } from 'react'
 
+
+
 const FitScreen = () => {
     const route = useRoute();
     console.log(route.params);
-    const [index,setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
     const excersise = route.params.excersises;
     const current = excersise[index];
-    console.log(current,'first excersise')
-  return (
-    <SafeAreaView>
-      <Text>FitScreen</Text>
-    </SafeAreaView>
-  )
+    console.log(current, 'first excersise')
+    return (
+        <SafeAreaView>
+            <Image
+                style={{
+                    width: '100%',
+                    height: 370
+                }}
+                source={{ uri: current.image }} />
+
+            <Text
+                style={{
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginTop: 30,
+                    fontSize: 30,
+                    fontWeight: "bold",
+                }}
+            >
+                {current.name}
+            </Text>
+            <Text
+                style={{
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginTop: 30,
+                    fontSize: 38,
+                    fontWeight: "bold",
+                }}
+            >
+                x{current.sets}
+            </Text>
+            <Pressable
+
+          style={{
+            backgroundColor: "white",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 30,
+            borderRadius: 20,
+            padding: 10,
+            width: 150,
+          }}
+        >
+             <Text
+            style={{
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 20,
+              color: "#292929",
+            }}
+          >
+            SUIVANT
+          </Text>
+        
+        </Pressable>
+        </SafeAreaView>
+    )
 }
 
 export default FitScreen
